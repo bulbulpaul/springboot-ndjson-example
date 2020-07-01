a8ec373c3bb58cb58bbcd6c11b6abbd98c22a5eb
bulbulpaul <bulbulpaul0325@gmail.com>
2020-07-01 22:33:34 +0900

    init

---
+++ src/main/kotlin/com/merrylab/example/ndjson/SpringbootNdjsonExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.merrylab.example.ndjson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootNdjsonExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootNdjsonExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/merrylab/example/ndjson/SpringbootNdjsonExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.merrylab.example.ndjson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootNdjsonExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
