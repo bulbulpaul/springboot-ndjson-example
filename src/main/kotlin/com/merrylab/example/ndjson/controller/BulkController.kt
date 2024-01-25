09717c709ac3205f1418c4a6fcc1b6366fda7bf5
Lucas León <lucasleon2107@gmail.com>
2024-01-25 03:05:13 -0500

    Use Flow and MediaType.APPLICATION_NDJSON_VALUE

--- src/main/kotlin/com/merrylab/example/ndjson/controller/BulkController.kt
+++ src/main/kotlin/com/merrylab/example/ndjson/controller/BulkController.kt
@@ -2,7 +2,9 @@ package com.merrylab.example.ndjson.controller
 
 import com.merrylab.example.ndjson.TodoService
 import com.merrylab.example.ndjson.domain.Todo
+import kotlinx.coroutines.flow.Flow
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,8 +20,8 @@ class BulkController(private val todoService: TodoService) {
         todoService.saveTodos(requestBody)
     }
 
-    @GetMapping("/todos", produces = ["application/x-ndjson"])
-    fun getTodos(): List<Todo> {
+    @GetMapping("/todos", produces = [MediaType.APPLICATION_NDJSON_VALUE])
+    fun getTodos(): Flow<Todo> {
         return todoService.getTodos()
     }
 }
